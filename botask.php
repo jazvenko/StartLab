1297c6a786b88e45be5052d2846c04b5363b1da3
jazvenko <jazvenko@gmail.com>
2017-04-28 23:37:28 +1000

    Initial commit

---
+++ botask.php
@@ -0,0 +1,16 @@
+<?php
+
+//ini_set('error_reporting', E_ALL);
+//ini_set('display_errors', 1);
+//ini_set('display_startup_errors', 1);
+
+$cmd = htmlspecialchars($_GET["cmd"]);
+$split = explode(' ', $cmd, 3);
+$json = array ('id'=>1,'name'=>"ivan",'country'=>'Russia',"office"=>array("yandex"," management"));
+$json2 = array ('id'=>$split[0],'name'=>$split[1],'country'=>$split[2],"office"=>array('id' => "123123",'id2' => "222222"));
+
+//print_r(array_diff(explode(' ', $cmd, 3),array("")));
+print_r(json_encode($json2, JSON_UNESCAPED_UNICODE));
+?>
+
+
